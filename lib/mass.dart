c7c700eff722487ba4ed9940bbe1d6c0a67de8c1
DAZA TOVAR JAIME <jdaza17@cuc.edu.co>
2023-10-06 22:09:41 -0500

    feat(bmi): Create basic BMI functionality

--- lib/mass.dart
+++ lib/mass.dart
@@ -8,18 +8,14 @@ class IMC extends StatefulWidget {
 }
 
 class _IMCState extends State<IMC> {
-  TextEditingController peso = TextEditingController();
-
-  TextEditingController altura = TextEditingController();
-
+  TextEditingController weightCtrl = TextEditingController();
+  TextEditingController heightCtrl = TextEditingController();
   double massCalculated = 0;
 
-  void Calcular() {
+  void calculate() {
     setState(() {
-      double p = double.parse(peso.text);
-
-      double a = double.parse(altura.text);
-
+      double p = double.parse(weightCtrl.text);
+      double a = double.parse(heightCtrl.text);
       massCalculated = (p / (a * a));
     });
   }
@@ -35,9 +31,7 @@ class _IMCState extends State<IMC> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Text(
               "Consulta tu Índice de Masa Corporal",
               style: TextStyle(
@@ -53,10 +47,32 @@ class _IMCState extends State<IMC> {
                   fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
-            Image.asset("assets/img/estado_imc.jpg")
+            Image.asset("assets/img/estado_imc.jpg", height: 200),
+            buildFields(weightCtrl, "Por favor, Digite su Peso (KG)",
+                TextInputType.number),
+            SizedBox(height: 20),
+            buildFields(heightCtrl, "Por favor, Digite su altura (Mts)",
+                TextInputType.number),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  calculate();
+                },
+                child: const Text("Calcular"),
+            ),
+            Text("SU INDICE DE MASA CORPORAL ES: $massCalculated"),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildFields(
+      TextEditingController ctrl, String label, TextInputType type) {
+    return TextField(
+      controller: ctrl,
+      keyboardType: type,
+      decoration: InputDecoration(labelText: label),
     );
   }
 }
